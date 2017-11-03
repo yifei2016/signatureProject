becc17e62dc98c9d083bf6942d56565c27fab829
Yifei Wang <hello.yifei@icloud.com>
2017-11-03 14:06:59 +0100

    finished version of mailsignature

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,132 +1,115 @@
 <template lang='html'>
   <div class="container">
+    <div>
       <div class="guide">
         <a href="https://linderoths.se/">
-          <img alt="Linderoths" class="logo" width="120.99" height="19.787" style="margin-bottom: 2%;" src="https://linderoths.se/wp-content/uploads/2016/12/linderoths_logo.png"/>
+          <img alt="Linderoths" width="120.99" height="19.787" style="margin-bottom: 18px;" src="https://linderoths.se/wp-content/uploads/2016/12/linderoths_logo.png" />
         </a>
       </div>
-      <div  style=" margin-bottom: 2em ;margin-top: 4%;">
+      <div style=" margin-bottom: 2em ;">
         <p class="guide" style="font-family: 'Andale Mono', monospace; font-size: 11px;" v-if="name || titel || businessArea">
-          <span style="color: #FC5961;"> • </span>
-          <span>&nbsp;</span>
-          <span style="color: #01215a;">{{name}}</span>
-          <span>&nbsp;</span>
-          <span style="color: #FC5961;"> • </span>
-          <span>&nbsp;</span>
-          <span style="color: #01215a;">{{titel}}</span>
-          <span>&nbsp;</span>
-          <span style="color: #FC5961;"> • </span>
-          <span>&nbsp;</span>
-          <span style="color: #01215a;">{{businessArea}}</span>
-          
+          <span class="marginToInfo" style="color: #01215a;">{{ name }}</span>
+          <span class="marginToInfo" style="color: #FC5961;">•</span>
+          <span class="marginToInfo" style="color: #01215a;">{{ titel }},</span>
+          <span style="color: #01215a;">{{ businessArea }}</span>
         </p>
-        <div class="guide">
-          <span style="color: #FC5961;" v-if="phone"> • </span>
-          <span>&nbsp;</span>
-          <a v-if="phone" style="color: #01215a;" :href="`tel:${phoneFormat}`">{{ phone }}</a>
-          <span>&nbsp;</span>
-          <span v-if="homepage" style="color: #FC5961;"> • </span>
-          <span>&nbsp;</span>
-          <a v-if="homepage" style="color: #FC5961;"  :href="`https://${hemsidan}`">{{ homepage }}</a>
-          <span>&nbsp;</span>
-          <span v-if="social" style="color: #FC5961;"> • </span>
-          <span>&nbsp;</span>
-          <span v-if="social" style="color: #01215a;">{{ social }}</span>
-      </div>
-      <div class="guide">
-    <span v-if="local" style="color: #FC5961;"> • </span>
-    <span>&nbsp;</span>
-    <span v-if="local" style="color: #01215a;" >{{ local }}</span>
-    <span>&nbsp;</span>
-    <span v-if="street" style="color: #FC5961;">•</span>
-    <span>&nbsp;</span>
-    <span v-if="street" style="color: #01215a;" >{{ street }}</span>
-    <span>&nbsp;</span>
-    <span v-if="postNumber" style="color: #FC5961;">•</span>
-    <span>&nbsp;</span>
-    <span v-if="postNumber" style="color: #01215a;">{{ postNumber }}</span>
-    <span>&nbsp;</span>
-    <span v-if="city" style="color: #01215a;">{{ city }}</span>
-      </div>
-         </div>
-
-    <div class="form">
-      <div class="form-group">
-        <div class="label guideFont ">
-          <label for="formGroupExampleInput">Name:</label>
-        </div>
-        <input type="text" v-model="name" v-on:keyup="setName" class="form-control" id="formGroupExampleInput" placeholder="Firstname Lastname">
-      </div>
-      <div class="form-group">
-        <div class="label guideFont">
-          <label for="formGroupExampleInput2">Work Title:</label>
-        </div>
-        <input type="text" v-model="titel" v-on:keyup="setTitel" class="form-control" id="" placeholder="Work Title">
-      </div>
-      <div class="form-group">
-        <div class="label guideFont">
-           <label for="formGroupExampleInput">Business Area:</label>
-        </div>
-        <input type="text" v-model="businessArea" class="form-control" v-on:keyup="setBusinessArea" id="formGroupExampleInput" placeholder="Business Area">
-      </div>
-      <div class="form-group">
-        <div class="label guideFont">
-           <label for="formGroupExampleInput">Phone Number:</label>
-        </div>
-        <input type="text" v-model="phone" class="form-control" id="formGroupExampleInput" v-on:keyup="setPhone" placeholder="+46 7XX XX XX XX">
-      </div>
-      <div class="form-group">
-        <div class="label guideFont">
-           <label for="formGroupExampleInput">Home Page:</label>
-        </div>
-        <input v-model="homepage" type="text" class="form-control" id="formGroupExampleInput" v-on:keyup="setHomepage" placeholder="https://linderoths.se">
-      </div>
-      <div class="form-group">
-        <div class="label guideFont">
-            <label for="formGroupExampleInput">Social Media:</label>
-        </div>
-        <input v-model="social" type="text" class="form-control" id="formGroupExampleInput" v-on:keyup="setSocial" placeholder="@wearelinderoths">
-      </div>
-      <div class="form-group">
-        <div class="label guideFont">
-           <label for="formGroupExampleInput">Location:</label>
-        </div>
-        <input v-model="local" type="text" class="form-control" id="formGroupExampleInput" v-on:keyup="setLocal" placeholder="Victoriapassagen">
-      </div>
-      <div class="form-group">
-        <div class="label guideFont">
-           <label for="formGroupExampleInput">Street:</label>
-        </div>
-        <input v-model="street" type="text" class="form-control" id="formGroupExampleInput" v-on:keyup="setStreet" placeholder="Södra Larmgatan 14">
-      </div>
-      <div class="form-group">
-        <div class="label guideFont">
-             <label for="formGroupExampleInput">Post Number:</label>
-        </div>
-        <input v-model="postNumber" type="text" class="form-control" id="formGroupExampleInput" v-on:keyup="setPostNumber" placeholder="411 16">
-      </div>
-      <div class="form-group">
-        <div class="label guideFont">
-            <label for="formGroupExampleInput">City:</label>
-        </div>
-        <input v-model="city" type="text" class="form-control" id="formGroupExampleInput" v-on:keyup="setCity" placeholder="Göteborg">
-      </div>
+        <div class="guide" style="font-family: 'Andale Mono', monospace; font-size: 11px; margin-bottom:1px;">
+          <span class="marginToInfo" style="color: #FC5961;" v-if="phone">•</span>
+          <a class="marginToInfo" v-if="phone" style="color: #01215a;text-decoration: none;" :href="`tel:${phoneFormat}`">{{ phone }}</a>
+        <span class="marginToInfo" v-if="homepage" style="color: #FC5961;"> • </span>
+        <a class="marginToInfo" v-if="homepage" style="color: #FC5961;"  :href="`https://${hemsidan}`">{{ homepage }}</a>
+      <span class="marginToInfo" v-if="social" style="color: #FC5961;"> • </span>
+      <span class="marginToInfo" v-if="social" style="color: #01215a;">{{ social }}</span>
     </div>
-    <div class="card guideFont guidecard">
-      <div class="card-block guide">
-        <h2 class="card-title guideColor">Guide:</h2>
-      </div>
-      <ul class="list-group list-group-flush">
-        <li class="list-group-item">Use Chrome</li>
-        <li class="list-group-item">Fill in the form</li>
-        <li class="list-group-item">Text select the signature</li>
-        <li class="list-group-item">Copy! Ctrl+C (pc) or CMD+C (mac)</li>
-        <li class="list-group-item">Open Mail app, goto settings, then mail signature settings</li>
-        <li class="list-group-item">Paste, save...</li>
-      </ul>
+    <div class="guide" style="font-family: 'Andale Mono', monospace; font-size: 11px;">
+      <span class="marginToInfo" v-if="local" style="color: #FC5961;"> • </span>
+      <span class="marginToInfo" v-if="local" style="color: #01215a;" >{{ local }}</span>
+      <span class="marginToInfo" v-if="street" style="color: #FC5961;">•</span>
+      <span class="marginToInfo" v-if="street" style="color: #01215a;" >{{ street }}</span>
+      <span class="marginToInfo" v-if="postNumber" style="color: #FC5961;">•</span>
+      <span class="marginToInfo" v-if="postNumber" style="color: #01215a;">{{ postNumber }}</span>
+      <span v-if="city" style="color: #01215a;">{{ city }}</span>
     </div>
   </div>
-</template>
+    </div>
+      
+  <div class="form">
+    <div class="form-group">
+      <div class="label guideFont ">
+        <label for="formGroupExampleInput">Name:</label>
+      </div>
+      <input type="text" v-model="name" v-on:keyup="setName" class="form-control" id="formGroupExampleInput" placeholder="Firstname Lastname">
+      </div>
+    <div class="form-group">
+      <div class="label guideFont">
+        <label for="formGroupExampleInput2">Work Title:</label>
+      </div>
+      <input type="text" v-model="titel" v-on:keyup="setTitel" class="form-control" id="" placeholder="Work Title">
+      </div>
+    <div class="form-group">
+      <div class="label guideFont">
+        <label for="formGroupExampleInput">Business Area:</label>
+      </div>
+      <input type="text" v-model="businessArea" class="form-control" v-on:keyup="setBusinessArea" id="formGroupExampleInput" placeholder="Business Area">
+      </div>
+    <div class="form-group">
+      <div class="label guideFont">
+        <label for="formGroupExampleInput">Phone Number:</label>
+      </div>
+      <input type="text" v-model="phone" class="form-control" id="formGroupExampleInput" v-on:keyup="setPhone" placeholder="+46 7XX XX XX XX">
+      </div>
+    <div class="form-group">
+      <div class="label guideFont">
+        <label for="formGroupExampleInput">Home Page:</label>
+      </div>
+      <input v-model="homepage" type="text" class="form-control" id="formGroupExampleInput" v-on:keyup="setHomepage" placeholder="https://linderoths.se">
+      </div>
+    <div class="form-group">
+      <div class="label guideFont">
+        <label for="formGroupExampleInput">Social Media:</label>
+      </div>
+      <input v-model="social" type="text" class="form-control" id="formGroupExampleInput" v-on:keyup="setSocial" placeholder="@wearelinderoths">
+      </div>
+    <div class="form-group">
+      <div class="label guideFont">
+        <label for="formGroupExampleInput">Location:</label>
+      </div>
+      <input v-model="local" type="text" class="form-control" id="formGroupExampleInput" v-on:keyup="setLocal" placeholder="Victoriapassagen">
+      </div>
+    <div class="form-group">
+      <div class="label guideFont">
+        <label for="formGroupExampleInput">Street:</label>
+      </div>
+      <input v-model="street" type="text" class="form-control" id="formGroupExampleInput" v-on:keyup="setStreet" placeholder="Södra Larmgatan 14">
+      </div>
+    <div class="form-group">
+      <div class="label guideFont">
+        <label for="formGroupExampleInput">Post Number:</label>
+      </div>
+      <input v-model="postNumber" type="text" class="form-control" id="formGroupExampleInput" v-on:keyup="setPostNumber" placeholder="411 16">
+      </div>
+    <div class="form-group">
+      <div class="label guideFont">
+        <label for="formGroupExampleInput">City:</label>
+      </div>
+      <input v-model="city" type="text" class="form-control" id="formGroupExampleInput" v-on:keyup="setCity" placeholder="Göteborg">
+      </div>
+  </div>
+  <div class="card guideFont guidecard">
+    <div class="card-block guide">
+      <h2 class="card-title guideColor">Guide:</h2>
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">Use Chrome</li>
+      <li class="list-group-item">Fill in the form</li>
+      <li class="list-group-item">Text select the signature</li>
+      <li class="list-group-item">Copy! Ctrl+C (pc) or CMD+C (mac)</li>
+      <li class="list-group-item">Open Mail app, goto settings, then mail signature settings</li>
+      <li class="list-group-item">Paste, save...</li>
+    </ul>
+  </div>
+  </div >
+</template >
 
 <script>
 export default {
